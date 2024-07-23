fa6be881646de0c5ce538f8e82c03ee0a2e99a4a
satish490 <satish.drupalchamp@gmail.com>
2024-07-23 19:07:40 +0530

    damami file uploaded

---
+++ web/sites/default/settings.php
@@ -0,0 +1,47 @@
+<?php
+
+/**
+ * Load services definition file.
+ */
+$settings['container_yamls'][] = __DIR__ . '/services.yml';
+
+/**
+ * Include the Pantheon-specific settings file.
+ *
+ * n.b. The settings.pantheon.php file makes some changes
+ *      that affect all environments that this site
+ *      exists in.  Always include this file, even in
+ *      a local development environment, to ensure that
+ *      the site settings remain consistent.
+ */
+include __DIR__ . "/settings.pantheon.php";
+
+/**
+ * Skipping permissions hardening will make scaffolding
+ * work better, but will also raise a warning when you
+ * install Drupal.
+ *
+ * https://www.drupal.org/project/drupal/issues/3091285
+ */
+// $settings['skip_permissions_hardening'] = TRUE;
+
+/**
+ * If there is a local settings file, then include it
+ */
+$local_settings = __DIR__ . "/settings.local.php";
+if (file_exists($local_settings)) {
+  include $local_settings;
+}
+$databases['default']['default'] = array (
+  'database' => 'damami',
+  'username' => 'root',
+  'password' => '',
+  'prefix' => '',
+  'host' => 'localhost',
+  'port' => '3306',
+  'namespace' => 'Drupal\\mysql\\Driver\\Database\\mysql',
+  'driver' => 'mysql',
+  'autoload' => 'core/modules/mysql/src/Driver/Database/mysql/',
+);
+$settings['hash_salt'] = 'lfnSMSU9N91s0chYfrbrVxtl4DmPyMWIob92p7dBtCFuIW-Mdvy3wvDyU6LkWckf01pfAjN5Dg';
+$config['system.logging']['error_level'] = 'verbose';
